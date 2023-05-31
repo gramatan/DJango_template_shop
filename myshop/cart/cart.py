f4fbcc74793db07972ccbebac4330f03056aaa04
gramatan <78417563+gramatan@users.noreply.github.com>
2023-05-31 15:16:35 +0600

    initial commit

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -34,3 +34,43 @@ class Cart:
         # чтобы обеспечить его сохранение
         self.session.modified = True
 
+    def remove(self, product):
+        """
+        Удалить товар из корзины.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        """
+        Прокрутить товарные позиции корзины в цикле и
+        получить товары из базы данных.
+        """
+        product_ids = self.cart.keys()
+        # получить объекты product и добавить их в корзину
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Подсчитать все товарные позиции в корзине.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        # удалить корзину из сеанса
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+    
