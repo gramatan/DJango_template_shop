7235b6f9d69b532f9ef49a16459231287122c5ed
gramatan <78417563+gramatan@users.noreply.github.com>
2023-06-05 15:18:23 +0600

    rabbitmq + celery commands in comments added

--- myshop/myshop/celery.py
+++ myshop/myshop/celery.py
@@ -1,4 +1,6 @@
 # docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:management
+# celery -A myshop worker -l info
+## http://127.0.0.1:15672/
 ## login - pass : guest - guest
 import os
 from celery import Celery
